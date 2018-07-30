4ae9d93be8211bbfb041f2b791e2cde2b36954ec
Anders Mörtberg <andersmortberg@gmail.com>
2018-07-30 16:40:06 +0200

    better error msgs in TC

--- TypeChecker.hs
+++ TypeChecker.hs
@@ -200,9 +200,16 @@ check a t = case (a,t) of
   (VPathP p a0 a1,PLam _ e) -> do
     (u0,u1) <- checkPLam p t
     ns <- asks names
-    unless (conv ns a0 u0 && conv ns a1 u1) $
-      throwError $ "path endpoints don't match for " ++ show e ++ ", got " ++
-                   show (u0,u1) ++ ", but expected " ++ show (a0,a1)
+    let (nu0,nu1) = normal ns (u0,u1)
+        (na0,na1) = normal ns (a0,a1)
+    unless (conv ns a0 u0) $
+      throwError $ "Left endpoints don't match for \n" ++ show e ++ "\ngot\n" ++
+                   show u0 ++ "\nbut expected\n" ++ show a0 ++
+                   "\n\nNormal forms:\n" ++ show nu0 ++ "\nand\n" ++ show na0
+    unless (conv ns a1 u1) $
+      throwError $ "Right endpoints don't match for \n" ++ show e ++ "\ngot\n" ++
+                   show u1 ++ "\nbut expected\n" ++ show a1 ++
+                   "\n\nNormal forms:\n" ++ show nu1 ++ "\nand\n" ++ show na1
   (VU,Glue a ts) -> do
     check VU a
     rho <- asks env
